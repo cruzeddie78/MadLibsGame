af8b58c34b4979d8ba62b95d992cd3be6b92bce6
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-09-12 18:15:05 -0700

    We added output statements so we can create some story using vairables and different data types

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,51 @@
 //Authors: 
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main()
 {
-
+        string a;
+        string b;
+        float d;
+        string c;
+        string e;
+        int f;
+        string g;
+        string h;
+        string i;
+   std::cout<<"Input a name:\n";
+   std::cin >> a;
+   std::cout<<"Input another name:\n";
+   std::cin >> b;
+   std::cout<<"Input a number or dollar amount:\n";
+   std::cin>> d;
+   std::cout<<"Input a noun:\n";
+   std::cin>> c;
+   std::cout<<"Input a name:\n";
+   std::cin>> e;
+   std::cout<<"Input a number:\n";
+   std::cin>>f;
+   std::cout<<"Input a noun:\n";
+   std::cin>> g;
+   std::cout<<"Input a verb:\n";
+   std::cin>> h;
+   std::cout<<"Input a verb:\n";
+   std::cin>> i;
+   std::cout<<"Mrs. ";
+   std::cout<<a;
+   std::cout<<" gave her son/daughter\n";
+   std::cout<< b << " ";
+   std::cout<< d << " ";
+   std::cout<<"to go buy ";
+   std::cout<<c;
+   std::cout<<" at\n";
+   std::cout<<e;
+   std::cout<<". On the way, he/she noticed\n";
+   std::cout<<f << " ";
+   std::cout<<g << " ";
+   std::cout<<h;
+   std::cout<<" over ";
+   std::cout<<i;
+   std::cout<<" and returned home.\n";
   return 0;
 }
